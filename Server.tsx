1bf0457ba0a0ff5b04d05b91ee7f5854b0935f58
rafaelmoczalla <Rafael.Moczalla@hpi.de>
2022-03-19 14:37:20 +0100

    Port settings should fit heroku requirements

--- Server.tsx
+++ Server.tsx
@@ -1,7 +1,7 @@
 import cors_proxy from 'cors-anywhere';
 
 // Listen on a specific host via the HOST environment variable
-var host = process.env.HOST || 'localhost';
+var host = process.env.PORT ? '0.0.0.0' : '127.0.0.1';
 // Listen on a specific port via the PORT environment variable
 var port = process.env.PORT || 5000;
 
