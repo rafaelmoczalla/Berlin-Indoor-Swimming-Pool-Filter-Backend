3fbd1576479219c05ccd2582019b2640ff6b9cc9
rafaelmoczalla <Rafael.Moczalla@hpi.de>
2022-03-19 14:49:55 +0100

    testing default cors-anywhere settings

--- Server.tsx
+++ Server.tsx
@@ -7,25 +7,8 @@ var port = process.env.PORT || 5000;
 
 cors_proxy.createServer({
     originWhitelist: [], // Allow all origins
-    removeHeaders: [
-      'cookie',
-      'cookie2',
-      // Strip Heroku-specific headers
-      'x-request-start',
-      'x-request-id',
-      'via',
-      'connect-time',
-      'total-route-time',
-      // Other Heroku added debug headers
-      // 'x-forwarded-for',
-      // 'x-forwarded-proto',
-      // 'x-forwarded-port',
-    ],
-    redirectSameOrigin: true,
-    httpProxyOptions: {
-      // Do not add X-Forwarded-For, etc. headers, because Heroku already adds it.
-      xfwd: false,
-    }
+    requireHeader: ['origin', 'x-requested-with'],
+    removeHeaders: ['cookie', 'cookie2']
 }).listen(port, host, function() {
     console.log('Running CORS Anywhere on ' + host + ':' + port);
 });
